9f63cebfa54bf3371d991723a24e527fdcef9bb9
owenbob <obyomuhangi@gmail.com>
2017-10-02 20:42:48 +0300

    Adding  basic models.py so as to obtain basic picture of the required tables for the database

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import *
+from models.models import User,Recipe
+#from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
