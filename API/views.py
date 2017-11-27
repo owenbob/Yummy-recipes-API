973b937167644fcc8df7c1b774adcb5ba81eff81
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2017-11-28 00:05:00 +0300

    Adding status codes to endpoint routes

--- API/views.py
+++ API/views.py
@@ -15,31 +15,38 @@ from API.models import User,Category,Recipe
 #Route for registering a user.This route takes the users details and assigns them a unique id
 @app.route("/register",methods=["POST"])
 def create_user():
+    
     if not request.json:
 
-        return jsonify({"message ":"Invalid Data Submitted"})
+        return jsonify({
+            "message ":"Invalid Data Submitted"
+            }),400
 
     user_info = request.get_json(force=True)
+    
     if user_info:
         hashed_password = generate_password_hash(user_info["password"], method="sha256")
 
         new_user = User(username=user_info["username"], email=user_info["email"], password=hashed_password)
-
         
+
         email_already_exists = db.session.query(db.exists().where(User.email == user_info["email"])).scalar()
         if email_already_exists:
-            return jsonify({"message":"This email has already been used to register"})
+            return jsonify({"message":"This email has already been used to register"}),400
         
         if user_info["username"] == "" or user_info["email"]== "" or user_info["password"]== "":
-            return jsonify({"message":"Please ensure you have input all your details"})
+            return jsonify({"message":"Please ensure you have input all your details"}),400
 
         if  not validate_email(user_info["email"]):
-            return jsonify({"message":"Please input correct email"})
+            return jsonify({"message":"Please input correct email"}),400
 
         db.session.add(new_user)
         db.session.commit()
 
-        return jsonify({"message" : "New user  has been created!"})
+        return jsonify({
+            "Status":"Success",
+            "message" : "New user  has been created!"
+            }),201
 
   
 
@@ -73,18 +80,18 @@ def login():
     auth = request.get_json()
 
     if not auth or not auth["username"] or not auth["password"]:
-        return  make_response("1.Could not verify")
+        return  make_response("1.Could not verify"),400
 
     user = User.query.filter_by(username=auth["username"]).first()
 
     if not user:
-        return make_response("2.Could not verify because provided details are not for user")
+        return make_response("2.Could not verify because provided details are not for user"),400
 
     if check_password_hash(user.password, auth["password"]):
         
         token = jwt.encode({"email" : user.email, "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes=50)}, app.config["SECRET_KEY"])
 
-        return jsonify({"token" : token.decode("UTF-8")})
+        return jsonify({"token" : token.decode("UTF-8")}),201
 
     return make_response("1.Could not verify")
 
@@ -93,7 +100,7 @@ def login():
 @token_needed
 def create_category(current_user):
     if not request.json:
-        return jsonify({"message ":"Invalid Data Submitted"})
+        return jsonify({"message ":"Invalid Data Submitted"}),400
     
     data = request.get_json()
 
@@ -104,12 +111,12 @@ def create_category(current_user):
         email=current_user.email
     )
     if data["category_title"]=="" or data["category_description"]== "":
-        return jsonify({"message":"Please ensure that you have input a category title and description"})
+        return jsonify({"message":"Please ensure that you have input a category title and description"}),400
 
     db.session.add(new_category)
     db.session.commit()
 
-    return jsonify({"message" : "Category created!"})
+    return jsonify({"message" : "Category created!"}),201
 
 
 
@@ -166,7 +173,7 @@ def get_one_category(current_user, category_id):
     category= Category.query.filter_by(category_id=category_id, email=current_user.email).first()
 
     if not category:
-        return jsonify({"message" : "No Category found!"})
+        return jsonify({"message" : "No Category found!"}),404
 
     category_data = {}
     category_data["category_id"] = category.category_id
@@ -182,18 +189,18 @@ def edit_category(current_user, category_id):
     category = Category.query.filter_by(category_id=category_id, email=current_user.email).first()
 
     if not  category:
-        return jsonify({"message" : "No Category found!"})
+        return jsonify({"message" : "No Category found!"}),404
     if not request.json:
-        return jsonify({"message ":"Invalid Data Submitted"})
+        return jsonify({"message ":"Invalid Data Submitted"}),400
 
     data = request.get_json()
     if data["category_description"] == "":
-        return jsonify({"message":"Please ensure that you have input a category description"})
+        return jsonify({"message":"Please ensure that you have input a category description"}),400
     category.category_title = data["category_title"]
     category.category_description=data["category_description"]
     db.session.commit()
 
-    return jsonify({"message" : "Category has been edited!"})
+    return jsonify({"message" : "Category has been edited!"}),201
 
 
 @app.route("/delete_category/<category_id>", methods=["DELETE"])
@@ -202,12 +209,12 @@ def delete_category(current_user, category_id):
     category = Category.query.filter_by(category_id=category_id, email=current_user.email).first()
 
     if not category :
-        return jsonify({"message" : "No Category found!"})
+        return jsonify({"message" : "No Category found!"}),404
 
     db.session.delete(category)
     db.session.commit()
 
-    return jsonify({"message" : "Category deleted!"})
+    return jsonify({"message" : "Category deleted!"}),200
 
 
 
@@ -217,9 +224,9 @@ def create_recipe(current_user,category_id):
 
     Available_category = Category.query.filter_by(category_id = category_id,email=current_user.email).first()
     if not  Available_category:
-        return({"message":"Category not available"}), 
+        return({"message":"Category not available"}),400
     if not request.json:
-        return jsonify({"message ":"Invalid Data Submitted"})
+        return jsonify({"message ":"Invalid Data Submitted"}),400
     
     data = request.get_json()
 
@@ -227,7 +234,7 @@ def create_recipe(current_user,category_id):
     if  data["recipe_title"] == "" or data["recipe_description"] == "":
         return jsonify({
             "message":"Please ensure that you have input a recipe_title,recipe_description and category title"
-            })
+            }),400
 
      
 
@@ -241,7 +248,7 @@ def create_recipe(current_user,category_id):
     db.session.add(new_recipe)
     db.session.commit()
 
-    return jsonify({"message" : "Recipe created!"})
+    return jsonify({"message" : "Recipe created!"}),201
 
 #Route to get all Recipes
 @app.route("/recipes", methods=["GET"])
@@ -263,7 +270,7 @@ def get_all_recipes(current_user):
                 recipe_data["recipe_description"] = recipe.recipe_description
                 output.append(recipe_data)
 
-            return jsonify({"Recipes" : output})
+            return jsonify({"Recipes" : output}),200
         
 
     if limit:
@@ -276,7 +283,7 @@ def get_all_recipes(current_user):
             recipe_data["recipe_description"] = recipe.recipe_description
             output.append(recipe_data)
 
-        return jsonify({"Recipes" : output})
+        return jsonify({"Recipes" : output}),200
                
     else:
         recipes = Recipe.query.filter_by(email=current_user.email).all()
@@ -288,7 +295,7 @@ def get_all_recipes(current_user):
             recipe_data["recipe_description"] = recipe.recipe_description
             output.append(recipe_data)
 
-        return jsonify({"Recipes" : output})
+        return jsonify({"Recipes" : output}),200
 
 
 
@@ -298,7 +305,7 @@ def get_one_recipe(current_user, recipe_id):
     recipe = Recipe.query.filter_by(recipe_id=recipe_id, email=current_user.email).first()
 
     if not recipe:
-        return jsonify({"message" : "No Recipe found!"})
+        return jsonify({"message" : "No Recipe found!"}),404
 
     recipe_data = {}
     recipe_data["recipe_id"] = recipe.recipe_id
@@ -315,17 +322,17 @@ def edit_recipe(current_user, recipe_id):
     recipe = Recipe.query.filter_by(recipe_id=recipe_id, email=current_user.email).first()
 
     if not  recipe:
-        return jsonify({"message" : "No Recipe found!"})
+        return jsonify({"message" : "No Recipe found!"}),404
 
     if not request.json:
-        return jsonify({"message ":"Invalid Data Submitted"})
+        return jsonify({"message ":"Invalid Data Submitted"}),400
         
     data = request.get_json()
     recipe.recipe_title =data["recipe_title"]
     recipe.recipe_description=data["recipe_description"]
     db.session.commit()
 
-    return jsonify({"message" : "Recipe has been edited!"})
+    return jsonify({"message" : "Recipe has been edited!"}),201
 
 @app.route("/delete_recipe/<recipe_id>", methods=["DELETE"])
 @token_needed
@@ -333,10 +340,10 @@ def delete_recipe(current_user, recipe_id):
     recipe = Recipe.query.filter_by(recipe_id=recipe_id, email=current_user.email).first()
 
     if not recipe:
-        return jsonify({"message" : "No Recipe found!"})
+        return jsonify({"message" : "No Recipe found!"}),404
 
     db.session.delete(recipe)
     db.session.commit()
 
-    return jsonify({"message" : "Recipe deleted!"})
+    return jsonify({"message" : "Recipe deleted!"}),200
 
