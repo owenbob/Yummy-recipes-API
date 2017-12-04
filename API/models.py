0984b702d03e3162630106e6482a4aa14cb2d803
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2017-12-04 18:41:37 +0300

    Adding check for special characters on API

--- API/models.py
+++ API/models.py
@@ -22,14 +22,16 @@ class User(db.Model):
     username = db.Column(db.String(50))
     email = db.Column(db.String(60),primary_key=True)
     password = db.Column(db.String(80))
+    user_date_stamp =db.Column(db.String(30))
     
 
 
-    def __init__(self,username,email, password):
+    def __init__(self,username,email, password,user_date_stamp):
         #initiliazing User class constructor
         self.username=username
         self.email = email
         self.password = password
+        self.user_date_stamp = user_date_stamp
 
          
         
@@ -48,15 +50,17 @@ class Category(db.Model):
     category_title = db.Column(db.String(30))
     category_description = db.Column(db.String(1000))
     email = db.Column(db.String(60))
+    category_date_stamp = db.Column(db.String(30))
 
 
 
-    def __init__(self, category_id,category_title, category_description, email):
+    def __init__(self, category_id,category_title, category_description, email,category_date_stamp):
         #initiliazing recipe class constructor
-        self.category_id=category_id
+        self.category_id = category_id
         self.category_title = category_title
         self.category_description = category_description
-        self.email= email
+        self.email = email
+        self.category_date_stamp = category_date_stamp
        
 
 
@@ -70,21 +74,26 @@ class Recipe(db.Model):
     
     __tablename__ = "Recipes"
 
-    recipe_id = db.Column(db.String(100), primary_key=True,)
+    recipe_id = db.Column(db.String(100), primary_key=True)
     recipe_title = db.Column(db.String(30))
     recipe_description = db.Column(db.String(1000))
     category_id = db.Column(db.String(100),db.ForeignKey('category.category_id'))
     category = db.relationship('Category',backref=db.backref('recipe', lazy=True))
     email = db.Column(db.String(60))
+    recipe_date_stamp = db.Column(db.String(30))
+    recipe_public_status = db.Column(db.Boolean)
     
 
-    def __init__(self, recipe_id, recipe_title,recipe_description,category_id,email):
+    def __init__(self, recipe_id, recipe_title,recipe_description,category_id,email,recipe_date_stamp,recipe_public_status):
         #initiliazing recipe class constructor
         self.recipe_id= recipe_id
         self.recipe_title = recipe_title
         self.recipe_description= recipe_description
         self.category_id =category_id
         self.email= email
+        self.recipe_date_stamp = recipe_date_stamp
+        self.recipe_public_status = recipe_public_status
+
        
 
 
--- API/views.py
+++ API/views.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 import jwt 
+import re
 from functools import wraps
 import uuid
 from validate_email import validate_email
@@ -19,6 +20,7 @@ def create_user():
     if not request.json:
 
         return jsonify({
+            "Status":"Fail",
             "message ":"Invalid Data Submitted"
             }),400
 
@@ -27,18 +29,40 @@ def create_user():
     if user_info:
         hashed_password = generate_password_hash(user_info["password"], method="sha256")
 
-        new_user = User(username=user_info["username"], email=user_info["email"], password=hashed_password)
+        new_user = User(
+            username=user_info["username"], 
+            email=user_info["email"], 
+            password=hashed_password,
+            user_date_stamp = str(datetime.datetime.now())
+            )
         
-
+        if  not re.match(
+            "^[A-Za-z0-9_-]*$", 
+            user_info['username']) or  not re.match(
+            "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$", 
+            user_info['email']):
+            return jsonify({
+                "Status":"Fail",
+                "message":"Please ensure you have not input special characters"
+            }),400
         email_already_exists = db.session.query(db.exists().where(User.email == user_info["email"])).scalar()
         if email_already_exists:
-            return jsonify({"message":"This email has already been used to register"}),400
+            return jsonify({
+                "Status":"Fail",
+                "message":"This email has already been used to register"
+                }),400
         
         if user_info["username"] == "" or user_info["email"]== "" or user_info["password"]== "":
-            return jsonify({"message":"Please ensure you have input all your details"}),400
+            return jsonify({
+                "Status":"Fail",
+                "message":"Please ensure you have input all your details"
+                }),400
 
         if  not validate_email(user_info["email"]):
-            return jsonify({"message":"Please input correct email"}),400
+            return jsonify({
+                "Status":"Fail",
+                "message":"Please input correct email"
+                }),400
 
         db.session.add(new_user)
         db.session.commit()
@@ -60,13 +84,18 @@ def token_needed(f):
             token = request.headers["x-access-token"]
 
         if not token:
-            return jsonify({"message" : "1.Token is missing!"}), 401
+            return jsonify({
+                "message" : "1.Token is missing!"
+                }), 401
 
         try: 
             data = jwt.decode(token, app.config["SECRET_KEY"])
             current_user = User.query.filter_by(email=data["email"]).first()
         except:
-            return jsonify({"message" : "2.Token is invalid!"}), 401
+            return jsonify({
+                "Status":"Fail",
+                "message" : "2.Token is invalid!"
+                }), 401
 
         return f(current_user, *args, **kwargs)
 
@@ -80,27 +109,39 @@ def login():
     auth = request.get_json()
 
     if not auth or not auth["username"] or not auth["password"]:
-        return  make_response("1.Could not verify"),400
+        return  make_response(
+            "1.Could not verify"
+            ),400
 
     user = User.query.filter_by(username=auth["username"]).first()
 
     if not user:
-        return make_response("2.Could not verify because provided details are not for user"),400
+        return make_response(
+            "2.Could not verify because provided details are not for user"
+            ),400
 
     if check_password_hash(user.password, auth["password"]):
         
-        token = jwt.encode({"email" : user.email, "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes=50)}, app.config["SECRET_KEY"])
+        token = jwt.encode({
+            "email" : user.email, 
+            "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes=50)}, 
+            app.config["SECRET_KEY"])
 
         return jsonify({"token" : token.decode("UTF-8")}),201
 
-    return make_response("1.Could not verify")
+    return make_response(
+        "1.Could not verify"
+        ),400
 
 #Endpoint to create category
 @app.route("/create_category", methods=["POST"])
 @token_needed
 def create_category(current_user):
     if not request.json:
-        return jsonify({"message ":"Invalid Data Submitted"}),400
+        return jsonify({
+            "Status":"Fail",
+            "message ":"Invalid Data Submitted"
+            }),400
     
     data = request.get_json()
 
@@ -108,15 +149,28 @@ def create_category(current_user):
         category_id=str(uuid.uuid4()),
         category_title=data["category_title"],
         category_description=data["category_description"],
-        email=current_user.email
+        email=current_user.email,
+        category_date_stamp =str(datetime.datetime.now())
     )
+    if  not re.match("^[A-Za-z0-9_-]*$", data["category_title"]) or  not re.match("^[A-Za-z0-9_-]*$", data["category_description"]):
+            return jsonify({
+                "Status":"Fail",
+                "message":"Please ensure you have not input special characters"
+            }),400
+
     if data["category_title"]=="" or data["category_description"]== "":
-        return jsonify({"message":"Please ensure that you have input a category title and description"}),400
+        return jsonify({
+            "Status":"Fail",
+            "message":"Please ensure that you have input a category title and description"
+            }),400
 
     db.session.add(new_category)
     db.session.commit()
 
-    return jsonify({"message" : "Category created!"}),201
+    return jsonify({
+        "Status":"Success",
+        "message" : "Category created!"
+        }),201
 
 
 
@@ -138,9 +192,13 @@ def get_all_categories(current_user):
                 category_data["category_id"] = category.category_id
                 category_data["category_title"] = category.category_title
                 category_data["category_description"] = category.category_description
+                category_data["Category_date_stamp"] = category.category_date_stamp
                 output.append(category_data)
 
-            return jsonify({"Categories" : output})
+            return jsonify({
+                "Status":"Success",
+                "Categories" : output
+                }),200
         
 
     if limit:
@@ -150,9 +208,13 @@ def get_all_categories(current_user):
             category_data["category_id"] = category.category_id
             category_data["category_title"] = category.category_title
             category_data["category_description"] = category.category_description
+            category_data["Category_date_stamp"] = category.category_date_stamp
             output.append(category_data)
 
-        return jsonify({"Categories" : output})
+        return jsonify({
+            "Status":"Success",
+            "Categories" : output
+            }),200
                
     else:
         categories = Category.query.filter_by(email=current_user.email).all()
@@ -161,9 +223,13 @@ def get_all_categories(current_user):
             category_data["category_id"] = category.category_id
             category_data["category_title"] = category.category_title
             category_data["category_description"] = category.category_description
+            category_data["Category_date_stamp"] = category.category_date_stamp
             output.append(category_data)
 
-        return jsonify({"Categories" : output})
+        return jsonify({
+            "Status":"Success",
+            "Categories" : output
+            }),200
 
 
 
@@ -173,12 +239,16 @@ def get_one_category(current_user, category_id):
     category= Category.query.filter_by(category_id=category_id, email=current_user.email).first()
 
     if not category:
-        return jsonify({"message" : "No Category found!"}),404
+        return jsonify({
+            "Status":"Fail",
+            "message" : "No Category found!"
+            }),404
 
     category_data = {}
     category_data["category_id"] = category.category_id
     category_data["category_title"] = category.category_title
     category_data["category_description"] = category.category_description
+    category_data["Category_date_stamp"] = category.category_date_stamp
 
     return jsonify(category_data)
 
@@ -189,18 +259,37 @@ def edit_category(current_user, category_id):
     category = Category.query.filter_by(category_id=category_id, email=current_user.email).first()
 
     if not  category:
-        return jsonify({"message" : "No Category found!"}),404
+        return jsonify({
+        "Status":"Fail",
+        "message" : "No Category found!"
+        }),404
+
     if not request.json:
-        return jsonify({"message ":"Invalid Data Submitted"}),400
+        return jsonify({
+            "Status":"Fail",
+            "message ":"Invalid Data Submitted"
+            }),400
 
     data = request.get_json()
-    if data["category_description"] == "":
-        return jsonify({"message":"Please ensure that you have input a category description"}),400
+    if data["category_title"] or data["category_description"] == "":
+        return jsonify({
+            "Status":"Fail",
+            "message":"Please ensure that you have input a category description"
+            }),400
+    if  not re.match("^[A-Za-z0-9_-]*$", data["category_title"]) or  not re.match("^[A-Za-z0-9_-]*$", data["category_description"]):
+            return jsonify({
+                "Status":"Fail",
+                "message":"Please ensure you have not input special characters"
+            }),400
+
     category.category_title = data["category_title"]
     category.category_description=data["category_description"]
     db.session.commit()
 
-    return jsonify({"message" : "Category has been edited!"}),201
+    return jsonify({
+        "Status":"Success",
+        "message" : "Category has been edited!"
+        }),201
 
 
 @app.route("/delete_category/<category_id>", methods=["DELETE"])
@@ -209,12 +298,18 @@ def delete_category(current_user, category_id):
     category = Category.query.filter_by(category_id=category_id, email=current_user.email).first()
 
     if not category :
-        return jsonify({"message" : "No Category found!"}),404
+        return jsonify({
+            "Status":"Fail",
+            "message" : "No Category found!"
+            }),404
 
     db.session.delete(category)
     db.session.commit()
 
-    return jsonify({"message" : "Category deleted!"}),200
+    return jsonify({
+        "Status":"Success",
+        "message" : "Category deleted!"
+        }),200
 
 
 
@@ -224,15 +319,27 @@ def create_recipe(current_user,category_id):
 
     Available_category = Category.query.filter_by(category_id = category_id,email=current_user.email).first()
     if not  Available_category:
-        return({"message":"Category not available"}),400
+        return({
+            "Status":"Fail",
+            "message":"Category not available"
+            }),400
     if not request.json:
-        return jsonify({"message ":"Invalid Data Submitted"}),400
+        return jsonify({
+            "Status":"Fail",
+            "message ":"Invalid Data Submitted"
+            }),400
     
     data = request.get_json()
+    if  not re.match("^[A-Za-z0-9_-]*$", data["recipe_title"]) or  not re.match("^[A-Za-z0-9_-]*$", data["recipe_description"]):
+            return jsonify({
+                "Status":"Fail",
+                "message":"Please ensure you have not input special characters"
+            }),400
 
 
     if  data["recipe_title"] == "" or data["recipe_description"] == "":
         return jsonify({
+            "Status":"Fail",
             "message":"Please ensure that you have input a recipe_title,recipe_description and category title"
             }),400
 
@@ -243,12 +350,18 @@ def create_recipe(current_user,category_id):
         recipe_title=data["recipe_title"],
         category_id=category_id,
         recipe_description=data["recipe_description"],
-        email=current_user.email)
+        recipe_public_status = False,
+        email=current_user.email,
+        recipe_date_stamp = str(datetime.datetime.now())
+        )
 
     db.session.add(new_recipe)
     db.session.commit()
 
-    return jsonify({"message" : "Recipe created!"}),201
+    return jsonify({
+        "Status":"Success",
+        "message" : "Recipe created!"
+        }),201
 
 #Route to get all Recipes
 @app.route("/recipes", methods=["GET"])
@@ -268,9 +381,14 @@ def get_all_recipes(current_user):
                 recipe_data["recipe_title"] = recipe.recipe_title
                 recipe_data["category_id"] =recipe.category_id
                 recipe_data["recipe_description"] = recipe.recipe_description
+                recipe_data["recipe_date_stamp"] = recipe.recipe_date_stamp
+                recipe_data["recipe_public_status"] = recipe.recipe_public_status
                 output.append(recipe_data)
 
-            return jsonify({"Recipes" : output}),200
+            return jsonify({
+                "Status":"Success",
+                "Recipes" : output
+                }),200
         
 
     if limit:
@@ -281,9 +399,14 @@ def get_all_recipes(current_user):
             recipe_data["recipe_title"] = recipe.recipe_title
             recipe_data["category_id"] =recipe.category_id
             recipe_data["recipe_description"] = recipe.recipe_description
+            recipe_data["recipe_date_stamp"] = recipe.recipe_date_stamp
+            recipe_data["recipe_public_status"] = recipe.recipe_public_status
             output.append(recipe_data)
 
-        return jsonify({"Recipes" : output}),200
+        return jsonify({
+            "Status":"Success",
+            "Recipes" : output
+            }),200
                
     else:
         recipes = Recipe.query.filter_by(email=current_user.email).all()
@@ -293,9 +416,14 @@ def get_all_recipes(current_user):
             recipe_data["recipe_title"] = recipe.recipe_title
             recipe_data["category_id"] =recipe.category_id
             recipe_data["recipe_description"] = recipe.recipe_description
+            recipe_data["recipe_date_stamp"] = recipe.recipe_date_stamp
+            recipe_data["recipe_public_status"] = recipe.recipe_public_status
             output.append(recipe_data)
 
-        return jsonify({"Recipes" : output}),200
+        return jsonify({
+            "Status":"Success",
+            "Recipes" : output
+            }),200
 
 
 
@@ -305,13 +433,18 @@ def get_one_recipe(current_user, recipe_id):
     recipe = Recipe.query.filter_by(recipe_id=recipe_id, email=current_user.email).first()
 
     if not recipe:
-        return jsonify({"message" : "No Recipe found!"}),404
+        return jsonify({
+            "Status":"Fail",
+            "message" : "No Recipe found!"
+            }),404
 
     recipe_data = {}
     recipe_data["recipe_id"] = recipe.recipe_id
     recipe_data["recipe_title"] = recipe.recipe_title
     recipe_data["category_id"] =recipe.category_id
     recipe_data["recipe_description"] = recipe.recipe_description
+    recipe_data["recipe_date_stamp"] = recipe.recipe_date_stamp
+    recipe_date["recipe_public_status"] = recipe.recipe_public_status
 
     return jsonify(recipe_data)
 
@@ -322,17 +455,51 @@ def edit_recipe(current_user, recipe_id):
     recipe = Recipe.query.filter_by(recipe_id=recipe_id, email=current_user.email).first()
 
     if not  recipe:
-        return jsonify({"message" : "No Recipe found!"}),404
+        return jsonify({
+        "Status":"Fail",
+        "message" : "No Recipe found!"
+        }),404
 
     if not request.json:
-        return jsonify({"message ":"Invalid Data Submitted"}),400
+        return jsonify({
+            "Status":"Fail",
+            "message ":"Invalid Data Submitted"
+            }),400
         
     data = request.get_json()
+    if  not re.match("^[A-Za-z0-9_-]*$", data["recipe_title"]) or  not re.match("^[A-Za-z0-9_-]*$", data["recipe_description"]):
+            return jsonify({
+                "Status":"Fail",
+                "message":"Please ensure you have not input special characters"
+            }),400
     recipe.recipe_title =data["recipe_title"]
     recipe.recipe_description=data["recipe_description"]
+    
     db.session.commit()
 
-    return jsonify({"message" : "Recipe has been edited!"}),201
+    return jsonify({
+        "Status":"Success",
+        "message" : "Recipe has been edited!"
+        }),201
+
+
+@app.route("/set_public_recipe/<recipe_id>",methods=["PUT"])
+@token_needed
+def set_public_recipe(current_user,recipe_id):
+    recipe = Recipe.query.filter_by(recipe_id=recipe_id, email=current_user.email).first()
+
+    if not recipe:
+        return jsonify({
+            "Status":"Fail",
+            "message" : "No Recipe found!"
+            }),404
+
+    recipe.recipe_public_status = True
+    db.session.commit()
+    return jsonify({
+        "Status":"Success",
+        "message" : "Recipe is now Public!"
+        }),201
 
 @app.route("/delete_recipe/<recipe_id>", methods=["DELETE"])
 @token_needed
@@ -340,10 +507,39 @@ def delete_recipe(current_user, recipe_id):
     recipe = Recipe.query.filter_by(recipe_id=recipe_id, email=current_user.email).first()
 
     if not recipe:
-        return jsonify({"message" : "No Recipe found!"}),404
+        return jsonify({
+            "Status":"Fail",
+            "message" : "No Recipe found!"
+            }),404
 
     db.session.delete(recipe)
     db.session.commit()
 
-    return jsonify({"message" : "Recipe deleted!"}),200
+    return jsonify({
+        "Status":"Success",
+        "message" : "Recipe deleted!"
+        }),200
+
+
+@app.route("/home",methods=["GET"])
+def public_recipes():
+    output=[]
+
+    recipes = Recipe.query.filter_by(recipe_public_status=True).all()
+    for recipe in recipes:
+        recipe_data = {}
+        recipe_data["recipe_id"] = recipe.recipe_id
+        recipe_data["recipe_title"] = recipe.recipe_title
+        recipe_data["category_id"] =recipe.category_id
+        recipe_data["recipe_description"] = recipe.recipe_description
+        recipe_data["recipe_date_stamp"] = recipe.recipe_date_stamp
+        recipe_data["recipe_public_status"] = recipe.recipe_public_status
+        output.append(recipe_data)
+
+    return jsonify({
+        "Status":"Success",
+        "Recipes" : output
+        }),200
+
+
 
