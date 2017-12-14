9899ea3f836801e1ef92280b794840de6086dda5
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2017-12-14 17:18:25 +0300

    Adding comments to to explain __init__.py

--- API/__init__.py
+++ API/__init__.py
@@ -1,3 +1,7 @@
+"""Declaring these in the __init__.py ensures that they are global 
+and the necessary modules can access them.
+"""
+
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
@@ -5,11 +9,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#configuring app secret key
 app.config["SECRET_KEY"] = "********"
 
 #directing API to databse yummy_recipes
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://yummyrecipes:admin@localhost:5432/yummy_recipes" or os.environ('DATABASE_URL')
-
-
 
 from API import views,models
