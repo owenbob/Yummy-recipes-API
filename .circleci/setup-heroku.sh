6a086225216dadd05726abe5943dc90527850daf
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2018-03-14 11:46:08 +0300

    Add setup-heroku

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -4,16 +4,16 @@
  sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
  sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
- cat > ~/.netrc << EOF
+  cat > ~/.netrc << EOF
  machine api.heroku.com
    login $HEROKU_LOGIN
    password $HEROKU_API_KEY
- EOF
+EOF
 
- cat >> ~/.ssh/config << EOF
+  cat >> ~/.ssh/config << EOF
  VerifyHostKeyDNS yes
  StrictHostKeyChecking no
- EOF
+EOF
 
  #Add heroku to the list of known hosts
 ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
