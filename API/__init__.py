6a0f592e02e065a9fb930601a605ca7f0600722d
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2018-03-20 19:17:47 +0300

    Add DB URL

--- API/__init__.py
+++ API/__init__.py
@@ -1,7 +1,6 @@
-"""Declaring these in the __init__.py ensures that they are global 
+"""Declaring these in the __init__.py ensures that they are global
 and the necessary modules can access them.
 """
-
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
@@ -9,10 +8,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#configuring app secret key
+# configuring app secret key
 app.config["SECRET_KEY"] = "********"
 
-#directing API to databse yummy_recipes
-app.config["SQLALCHEMY_DATABASE_URI"] =  os.environ.get('DATABASE_URL')
-# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://yummyrecipes:admin@localhost:5432/yummy_recipes" or os.environ.get('DATABASE_URL')  
-from API import views,models
+# directing API to databse yummy_recipes
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://yummyrecipes:admin@localhost:5432/yummy_recipes" or os.environ.get('DATABASE_URL')
+
+from API import views, models
